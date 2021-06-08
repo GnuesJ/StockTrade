148d23f7cbaed3420ee7976911c0e4e4ccb1ad79
GnuesJ <48038447+GnuesJ@users.noreply.github.com>
2021-06-08 11:45:43 -0400

    create npm dependencies

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+// import css from "application.css";
+
+//creates bundle and configures server
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path:path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  //configures the server
+  devServer: {
+    publicPath: '/build/',
+    proxy: {
+      '/api/leaders': 'http://localhost:3000',
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        // use: [
+        //   {loader: 'style-loader'},
+        //   {loader: "css-loader"}, 
+        //   {loader: "sass-loader"}
+        // ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        }
+    ]
+  }
+}
+
+
+/*
+
+
+*/
