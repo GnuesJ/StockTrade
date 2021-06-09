43a1d1ee6dbe8a95f9db876a579764619982943a
GnuesJ <48038447+GnuesJ@users.noreply.github.com>
2021-06-09 02:11:55 -0400

    start frontend

---
+++ client/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import MainContainer from './containers/MainContainer.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>hello
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//<MainContainer />
